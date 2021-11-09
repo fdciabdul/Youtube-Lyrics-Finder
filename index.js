b57661487a1f841156722237ca07de6d966a9d0e
Abdul ムッタキン <abdulmuttaqin456@gmail.com>
2021-11-10 03:47:11 +0700

    remove

--- index.js
+++ index.js
@@ -14,21 +14,7 @@ module.exports = {
         throw err;
       }
     }
-    // searchLyricsByKeywords: async(keyword) =>{
-    //     try {
-    //         const word = await yt.searchVideo(keyword);
-    //         const artist = await yt.getArtist(word[0].id);
-    //         const lirik = await yt.searchLyric(artist.song , artist.artist)
-    //         return {
-    //             song : artist.song , 
-    //             artist: artist.artist,
-    //             lyrics : lirik
     
-    //         }
-    //     } catch (error) {
-    //         throw error;
-    //     }
-    // }
 
 }
   
